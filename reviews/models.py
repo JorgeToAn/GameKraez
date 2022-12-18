b611c2305a5b0812a4b54e3c9c936236c7678f57
Mark0rtiz <106716676+Mark0rtiz@users.noreply.github.com>
2022-12-18 11:56:46 -0600

    review2

--- reviews/models.py
+++ reviews/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.urls import reverse 
+from orders.models import Product
 
 # Create your models here.
 class Status(models.Model):
@@ -22,6 +23,12 @@ class Review(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.ForeignKey(
         Status, 
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True
+    )
+    product = models.ForeignKey(
+        Product, 
         on_delete=models.CASCADE,
         blank=True,
         null=True
